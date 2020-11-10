1a662ef95c9cebb82b837694eca2e06d05e69c61
Garth Slaney <45348251+GarthS1@users.noreply.github.com>
2020-11-10 16:18:18 -0700

    Switch Bewteen Images
    You are now able to switch from and to map, cam1 and cam2 images. When we have the streams this will be replaced but the moment is a good way to decide layout.

---
+++ webGui/map.js
@@ -0,0 +1,18 @@
+
+function add_map() {
+    var src = "https://images-na.ssl-images-amazon.com/images/I/61GaHo-zgQL._AC_SL1000_.jpg";
+    show_image(src, 2500,1000, "Map");
+}
+
+//$(document).ready(function() {
+//    document.getElementsByClassName("myImg")[0].src = "hackanm.gif";
+//});
+
+function show_image(src, width, height, alt) {
+    var img = document.createElement("img");
+    img.src = src;
+    img.width = width;
+    img.height = height;
+    img.alt = alt;
+    document.body.appendChild(img);
+}
