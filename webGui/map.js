2a84da3d63bf510ac7818d7f2e4232e5bada80d2
Garth Slaney <45348251+GarthS1@users.noreply.github.com>
2020-11-19 16:41:10 -0700

    Overlapping
    Added changing overlapping images and put all the stlye in a css file.

--- webGui/map.js
+++ webGui/map.js
@@ -1,5 +1,5 @@
 
-function add_map() {
+/*function add_map() {
     var src = "https://images-na.ssl-images-amazon.com/images/I/61GaHo-zgQL._AC_SL1000_.jpg";
     show_image(src, 2500,1000, "Map");
 }
@@ -15,4 +15,22 @@ function show_image(src, width, height, alt) {
     img.height = height;
     img.alt = alt;
     document.body.appendChild(img);
+}*/
+
+function showMap(){
+  document.getElementById('bigImage').src='https://images-na.ssl-images-amazon.com/images/I/61GaHo-zgQL._AC_SL1000_.jpg';
+  document.getElementById('small1Image').src='https://i.ytimg.com/vi/z4ZLkyTuX_w/maxresdefault.jpg';
+  document.getElementById('small2Image').src='https://image.freepik.com/free-photo/man-is-sitting-wooden-bench-he-looks-sea-back-view_183270-78.jpg';
+}
+
+function showView1(){
+  document.getElementById('bigImage').src='https://i.ytimg.com/vi/z4ZLkyTuX_w/maxresdefault.jpg';
+  document.getElementById('small1Image').src='https://images-na.ssl-images-amazon.com/images/I/61GaHo-zgQL._AC_SL1000_.jpg';
+  document.getElementById('small2Image').src='https://image.freepik.com/free-photo/man-is-sitting-wooden-bench-he-looks-sea-back-view_183270-78.jpg';
+}
+
+function showView2(){
+  document.getElementById('bigImage').src='https://image.freepik.com/free-photo/man-is-sitting-wooden-bench-he-looks-sea-back-view_183270-78.jpg';
+  document.getElementById('small1Image').src='https://images-na.ssl-images-amazon.com/images/I/61GaHo-zgQL._AC_SL1000_.jpg';
+  document.getElementById('small2Image').src='https://i.ytimg.com/vi/z4ZLkyTuX_w/maxresdefault.jpg';
 }
