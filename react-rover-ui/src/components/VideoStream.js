c5a0785e8efd18ae0858b4aa853c1b938920077f
derek-name <derek.braun.99@gmail.com>
2021-05-13 23:22:25 -0600

    Put the mjpegcanvas source files inside the src directory

--- react-rover-ui/src/components/VideoStream.js
+++ react-rover-ui/src/components/VideoStream.js
@@ -3,7 +3,7 @@ import ROSLIB from 'roslib';
 import { FormControl, Select, MenuItem } from "@material-ui/core";
 import { makeStyles } from '@material-ui/core/styles';
 
-import { MJPEGCANVAS } from '../../mjpegcanvas/build/mjpegcanvas.js';
+import { MJPEGCANVAS } from '../mjpegcanvas/build/mjpegcanvas.js';
 
 const useStyles = makeStyles((theme) => ({
   formControl: {
