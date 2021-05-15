426fd57d7d750f5de688c42e8b27e2b9f4581454
derek-name <derek.braun.99@gmail.com>
2021-05-14 20:21:46 -0600

    Add more formatting and size video stream

--- react-rover-ui/src/App.js
+++ react-rover-ui/src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
 import { launchConsts } from "./setup/LaunchConsts";
 import { MapUiContainer } from "./containers/MapUiContainer";
+import { VideoStream } from "./components/VideoStream";
 
 function App() {
   return <div className="primary-container">
     <div className="data-column"></div>
     <div className="map-view"><MapUiContainer launchConsts={launchConsts}/></div>
-    <div className="video-feed-a"></div>
-    <div className="video-feed-b"></div>
+    <div className="video-feed-a"><VideoStream /></div>
+    <div className="video-feed-b"><VideoStream /></div>
     <div className="plan-panel"></div>
+    <div className="control-panel"></div>
   </div>
 }
 
--- react-rover-ui/src/components/VideoStream.js
+++ react-rover-ui/src/components/VideoStream.js
@@ -1,57 +1,48 @@
-// import React, { Component } from 'react';
-// import ROSLIB from 'roslib';
-// import { FormControl, Select, MenuItem } from "@material-ui/core";
-// import { makeStyles } from '@material-ui/core/styles';
+import React, { Component } from 'react';
+import ROSLIB from 'roslib';
+import { FormControl, Select, MenuItem } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
 
-// import { MJPEGCANVAS } from '../mjpegcanvas/build/mjpegcanvas.js';
+import { MJPEGCANVAS } from '../mjpegcanvas/build/mjpegcanvas.js';
 
-// const useStyles = makeStyles((theme) => ({
-//   formControl: {
-//     margin: theme.spacing(1),
-//     minWidth: 120,
-//   },
-//   selectEmpty: {
-//     marginTop: theme.spacing(2),
-//   },
-// }));
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
 
-// export class VideoStream extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//     	feedId: 1,
-//     };
-//     var classes = useStyles();
-//   }
+export class VideoStream extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    	feedId: 1,
+    };
+  }
 
-//   componentDidMount() {
-//     var viewer = new MJPEGCANVAS.Viewer({
-//       divID : 'mjpeg',
-//       host : 'localhost',
-//       height : 480,
-//       width : 500,
-//       topic : '/wide_stereo/left/image_color'
-//     });
-//   }
+  componentDidMount() {
+    var viewer = new MJPEGCANVAS.Viewer({
+      divID : 'mjpeg',
+      host : 'localhost',
+      height : this.refs.child.parentNode.clientHeight,
+      width : this.refs.child.parentNode.clientWidth,
+      topic : '/wide_stereo/left/image_color'
+    });
+  }
 
-//   render() {
-//     return (
-//     	<div>
-// 			<div id='mjpeg'/>
-// 			<FormControl className={classes.formControl}>
-// 		        <InputLabel id="demo-simple-select-label">Age</InputLabel>
-// 		        <Select
-// 		          labelId="demo-simple-select-label"
-// 		          id="demo-simple-select"
-// 		          value={age}
-// 		          onChange={handleChange}
-// 		        >
-// 		          <MenuItem value={1}>One</MenuItem>
-// 		          <MenuItem value={2}>Two</MenuItem>
-// 		          <MenuItem value={3}>Three</MenuItem>
-// 		        </Select>
-// 		      </FormControl>
-// 		</div>
-//     );
-//   }
-// }
+  render() {
+    return (
+			  <div id='mjpeg' ref="child">
+          <select style={{position: 'absolute', marginTop: '10px', marginLeft: '10px', paddingRight: '30px'}} name="feeds" id="feeds">
+            <option value="front">Front</option>
+            <option value="arm">Arm</option>
+            <option value="science">Science</option>
+            <option value="none">None</option>
+          </select>
+        </div>
+    );
+  }
+}
