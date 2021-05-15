0e113f6d287cff64c4d34719e05f0341c4431215
derek-braun <derek.braun.99@gmail.com>
2021-05-15 10:24:51 -0600

    Add multi stream canvas to layout

--- react-rover-ui/src/App.js
+++ react-rover-ui/src/App.js
@@ -2,13 +2,14 @@ import "./App.css";
 import { launchConsts } from "./setup/LaunchConsts";
 import { MapUiContainer } from "./containers/MapUiContainer";
 import { VideoStream } from "./components/VideoStream";
+import { MultiVideoStream } from "./components/MultiVideoStream";
 
 function App() {
   return <div className="primary-container">
     <div className="data-column"></div>
     <div className="map-view"><MapUiContainer launchConsts={launchConsts}/></div>
-    <div className="video-feed-a"><VideoStream /></div>
-    <div className="video-feed-b"><VideoStream /></div>
+    <div className="video-feed-a"><VideoStream selection="front"/></div>
+    <div className="video-feed-b"><MultiVideoStream/></div>
     <div className="plan-panel"></div>
     <div className="control-panel"></div>
   </div>
--- react-rover-ui/src/components/MultiVideoStream.js
+++ react-rover-ui/src/components/MultiVideoStream.js
@@ -3,27 +3,25 @@ import ROSLIB from 'roslib';
 
 import { MJPEGCANVAS } from '../mjpegcanvas/build/mjpegcanvas.js';
 
-const topicMap = {"front": "/camera/image_raw"};
-
-export class VideoStream extends Component {
+export class MultiVideoStream extends Component {
   constructor(props) {
     super(props);
-
   }
 
   componentDidMount() {
-    var viewer = new MJPEGCANVAS.Viewer({
+    var viewer = new MJPEGCANVAS.MultiStreamViewer({
       divID : 'mjpeg',
       host : 'localhost',
       height : this.refs.child.parentNode.clientHeight,
       width : this.refs.child.parentNode.clientWidth,
-      topic : topicMap[this.props.selection]
+      topics : ['/camera/image_raw', '/camera/image_raw/theora', 'topic', ''],
+      labels : ['front', 'arm', 'science', 'none']
     });
   }
 
   render() {
     return (
-		<div id='mjpeg' ref="child" />
+		  <div id='mjpeg' ref="child" />
     );
   }
 }
