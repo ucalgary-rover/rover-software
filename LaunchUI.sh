c5503fcd7acb7b93e2f6707a00308c5366adb4ae
derek-braun <derek.braun.99@gmail.com>
2021-06-14 17:41:29 -0600

    Update README with launch script instructions

--- LaunchUI.sh
+++ LaunchUI.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 #Launch the React UI and Express Image Server
 
-gnome-terminal --tab -- bash -c 'roscore'
-gnome-terminal --tab -- bash -c 'roslaunch rosbridge_server rosbridge_websocket.launch'
 gnome-terminal --tab -- bash -c 'cd express-image-server; npm start'
 gnome-terminal --tab -- bash -c 'cd react-rover-ui; npm start'
