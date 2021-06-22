ed6317ec652ef631645f1a69c67141e3cd271fb7
waleed77777 <waleed.420@yahoo.com>
2021-06-22 07:44:31 +0400

    Improve MockGps

--- ros-workspace/src/rover/mocks/MockGps.cpp
+++ ros-workspace/src/rover/mocks/MockGps.cpp
@@ -1,7 +1,7 @@
 // Simulates periodic gps location messages
 
 #include <ros/ros.h>
-
+#include <math.h>
 #include <rover/GpsCoords.h>
 #include <stdlib.h>
 
@@ -21,16 +21,47 @@ int main(int argc, char **argv)
 	srand(time(0));
 
 	ros::Rate rate(1);
+
+
+	double previousLat=latParam;
+	double previousLon=lonParam;
+
+	double latDisplacment= (((float)rand()-RAND_MAX/2)/(float)(RAND_MAX/3.0));  //for constanst speed
+	double lonDisplacment= (((float)rand()-RAND_MAX/2)/(float)(RAND_MAX/3.0));
+	
 	while(ros::ok())
 	{
 		rover::GpsCoords msg;
 
 		msg.header.stamp = ros::Time::now();
 
-		msg.latitude = double(rand());
-		msg.longitude = double(rand());
+		//double latDisplacment= (((float)rand()-RAND_MAX/2)/(float)(RAND_MAX/3.0)); // for random speed
+		//double lonDisplacment= (((float)rand()-RAND_MAX/2)/(float)(RAND_MAX/3.0));
+		
+		msg.latitude= previousLat;
+		msg.longitude= previousLon;
+		
+		previousLat +=  latDisplacment;
+		previousLon += lonDisplacment;
+
+		msg.ground_speed = sqrt(pow(latDisplacment,2)+ pow(lonDisplacment,2));
+
+		/*if (msg.latitude>0 && msg.longitude>0)
+		{
+			msg.direction = "NE";
+		} else if (msg.latitude<0 && msg.longitude>0)
+		{
+			msg.direction = "SE";
+		}else if (msg.latitude<0 && msg.longitude<0)			// needing to know how to access msg board
+		{
+			msg.direction = "SW";
+		}else if (msg.latitude>0 && msg.longitude<0)
+		{
+			msg.direction = "NW";
+		}*/
 
 		pub.publish(msg);
+
 
 		ROS_INFO_STREAM("Got Parameters: " << latParam << ", " << lonParam);
 
